382207e842c4dd66cf954b1e9987bc0ae0a7fc0b
miltonclary <93954209+miltonclary@users.noreply.github.com>
2025-07-08 19:17:16 +0000

    server stuff

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 const DATA_FILE = path.join(__dirname, 'data.json');
 
 // Middleware
